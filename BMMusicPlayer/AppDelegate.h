b721796fe636aaf8ea9f771e6f524adab3b8bf59
229953445 <229953445@qq.com>
2018-11-15 01:23:59 +0800

    初始化项目

---
+++ BMMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMMusicPlayer
+//
+//  Created by BirdMichael on 2018/11/15.
+//  Copyright © 2018 BirdMichael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMMusicPlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BMMusicPlayer
+//
+//  Created by BirdMichael on 2018/11/15.
+//  Copyright © 2018 BirdMichael. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
